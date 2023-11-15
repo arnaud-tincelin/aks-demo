c9f5c48a04f7e2aba77df855ea759322dcf17041
Mohamed Soumare <soumare@outlook.com>
2023-11-15 17:29:40 +0000

    Updated the projects

--- howtoaks/HowToAKS.Web/Controllers/HomeController.cs
+++ howtoaks/HowToAKS.Web/Controllers/HomeController.cs
@@ -1,45 +1,27 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Diagnostics;
-using System.Linq;
-using System.Threading.Tasks;
+﻿using HowToAKS.Web.Models;
 using Microsoft.AspNetCore.Mvc;
-using HowToAKS.Web.Models;
-using System.Net.Http;
+using System.Diagnostics;
 
 namespace HowToAKS.Web.Controllers
 {
     public class HomeController : Controller
     {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
         public IActionResult Index()
         {
             return View();
         }
 
-        public async Task<IActionResult> Test()
+        public IActionResult Privacy()
         {
-            try  
-            {
-                using (var client = new HttpClient())
-                {
-                    var request = new HttpRequestMessage
-                    {
-                        RequestUri = new Uri("http://howtoaks-webapi:8081/api/values") // When deploying in the default namespace, then you only use the container/pod name in the dns.                        
-                        // RequestUri = new Uri("http://howtoaks-webapi.back-end:8081/api/values") // When deploying in custom namespaces, then you must include the namespace in the dns.
-                    };
-
-                    var response = await client.SendAsync(request);
-                    ViewBag.Result = await response.Content.ReadAsStringAsync();
-                }
-            }
-            catch (Exception ex)
-            {
-                ViewBag.Result = ex;
-            }
-
             return View();
         }
-
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
