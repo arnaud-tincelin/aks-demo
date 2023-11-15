00dbc970d9131b0f0ea77fbd77de0b347dd114fa
Mohamed Soumare <soumare@outlook.com>
2023-11-15 17:35:16 +0000

    added code to test communication between WebApp to WebApi

--- howtoaks/HowToAKS.Web/Controllers/HomeController.cs
+++ howtoaks/HowToAKS.Web/Controllers/HomeController.cs
@@ -18,6 +18,30 @@ namespace HowToAKS.Web.Controllers
             return View();
         }
 
+        public async Task<IActionResult> Test()
+        {
+            try
+            {
+                using (var client = new HttpClient())
+                {
+                    var request = new HttpRequestMessage
+                    {
+                        RequestUri = new Uri("http://howtoaks-webapi:8081/api/values") // When deploying in the default namespace, then you only use the container/pod name in the dns.                        
+                        // RequestUri = new Uri("http://howtoaks-webapi.back-end:8081/api/values") // When deploying in custom namespaces, then you must include the namespace in the dns.
+                    };
+
+                    var response = await client.SendAsync(request);
+                    ViewBag.Result = await response.Content.ReadAsStringAsync();
+                }
+            }
+            catch (Exception ex)
+            {
+                ViewBag.Result = ex;
+            }
+
+            return View();
+        }
+
         public IActionResult Privacy()
         {
             return View();
